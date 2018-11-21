4b74c2a205373c660ed99d047f0732665e164788
redth <jondick@gmail.com>
2018-11-21 14:22:45 -0500

    Commit typeforwards from GenAPI directly
    GenAPI is a very brittle tool, especially to have run xplat.  Since we don't anticipate doing these typeforwards again as Android Support 28.x is the last support library version that will be released, we are going to just manually commit the generated typeforwards this one time, and make it so that msbuild target won't actually run on build.  This resolves some headaches of trying to make something work that won't be needed (at least in the same way) again.

---
+++ source/com.android.support/support-v4/additions/Xamarin.Android.Support.Core.UI.cs
@@ -0,0 +1,2 @@
+[assembly:System.Runtime.CompilerServices.TypeForwardedTo(typeof(Android.Support.V4.App.ActionBarDrawerToggle))]
+[assembly:System.Runtime.CompilerServices.TypeForwardedTo(typeof(Android.Support.V4.Widget.Space))]
