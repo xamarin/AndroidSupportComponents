9c043e17dd20e6616b010f98fe5e0a5a3e0adcf2
Matthew Leibowitz <mattleibow@live.com>
2019-06-19 00:24:05 +0200

    Some more changes

---
+++ source/androidx.fragment/fragment/additions/Additions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections;
+using Android.Runtime;
+
+namespace AndroidX.Fragment.App
+{
+	public partial class FragmentActivity
+	{
+		public override AndroidX.Lifecycle.Lifecycle Lifecycle => base.Lifecycle;
+	}
+}
